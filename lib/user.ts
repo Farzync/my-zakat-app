bba6d33f759e8fa8c07790ae3a121a6b0e145615
Faeza Raziq <faezaraziqg@gmail.com>
2025-06-29 13:39:41 +0700

    refactor(prisma): extract Prisma client to shared module

--- lib/user.ts
+++ lib/user.ts
@@ -1,6 +1,4 @@
-import { PrismaClient } from "@prisma/client"
-
-const prisma = new PrismaClient()
+import { prisma } from "@/lib/prisma";
 
 export async function getUserByCredentials(username: string, password: string) {
   return await prisma.user.findFirst({
